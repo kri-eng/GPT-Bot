d464c4ab84cd05f80d41699771840896eb6b300d
Krish Patel <124129235+kri-eng@users.noreply.github.com>
2024-03-26 16:08:31 -0500

    Update APIInfo.swift

--- DemoGPT/APIInfo.swift
+++ DemoGPT/APIInfo.swift
@@ -10,5 +10,6 @@ import Foundation
 struct APIInfo {
     
 //    static let apiKey: String = "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"
+// disabaled so use yours please.
     static let apiKey: String = "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"
 }
